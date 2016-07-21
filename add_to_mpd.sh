5819d2d8c13307fabae7f35ce74614df057d6401
rbn42 <bl100@students.waikato.ac.nz>
2016-07-22 08:30:25 +1200

    initial commit

---
+++ add_to_mpd.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mpc add $(youtube-dl -g -f140 "$1")
---
+++ music_list.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export ADDTOMPD=$(dirname "$0")/add_to_mpd.sh
+mpc clear
+
+bash $ADDTOMPD "p2vxlZkfW-Y" #Record of Lodoss War - opening eternity
+bash $ADDTOMPD "eJiKEy4rsvA" #Record of Lodoss War - opening  ?
+
+mpc play
+
