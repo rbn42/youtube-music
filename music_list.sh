1f80e4682d0bc9c26e07cf5506970ed6a6a5d02f
rbn42 <bl100@students.waikato.ac.nz>
2016-07-28 07:35:16 +1200

    music_list.sh

--- music_list.sh
+++ music_list.sh
@@ -6,6 +6,22 @@ bash $ADDTOMPD "p2vxlZkfW-Y" #Record of Lodoss War - opening eternity
 bash $ADDTOMPD "eJiKEy4rsvA" #Record of Lodoss War - opening  ?
 bash $ADDTOMPD "aoEvKKAhSqQ" #Sidonia (Knights of Sidonia Theme Song)
 bash $ADDTOMPD "2IazvA6R0pY" #進撃の巨人 OP 2 FULL- Shingeki no Kyojin Opening 2 FULL HD
+bash $ADDTOMPD "I0fc-3-PXSo" #Shingeki no Kyojin ( Attack on Titan ) Opening 1
+bash $ADDTOMPD "cV2LfNl5eO8" #The Everlasting Guilty Crown [EGOIST] with Lyrics
+bash $ADDTOMPD "581yuUMGQPE" #BRYNHILDR IN THE DARKNESS Ver EJECTED
+bash $ADDTOMPD "_9GUvFPLUQU" #Black Bullet Opening Full
+bash $ADDTOMPD "CaksNlNniis" #No Game No Life Opening Full
+bash $ADDTOMPD "XWmj6E3jhHY" #Rail Wars! Opening 1 Full
+bash $ADDTOMPD "XWmj6E3jhHY" #Rail Wars! Opening 1 Full
+bash $ADDTOMPD "OcFbq8WGfZE" #Guilty Crown Opening 1 Full "My Dearest"
+bash $ADDTOMPD "AKmCATB9Ab8" #Kuusou Mesorogiwi - Mirai Nikki OP 1 FULL (ENGLISH Lyrics)
+bash $ADDTOMPD "Qd_SAQvm69E" #Kore wa Zombie Desu ka? Full OP (Ma-Ka-Se-Te Tonight) 
+bash $ADDTOMPD "v6iAOXC9_Sk" #PSYCHO-PASS OP " abnormalize " 弾いてみた/abnormalize - Ling Tosite Sigure (Guitar cover by 水町涼)
+bash $ADDTOMPD "r7bMnrHdtYI" #Shinseiki Evangelion - Opening (Zankoku na Tenshi no These)_Sub Thai
+bash $ADDTOMPD "aNQvLECht08" #Tengen Toppa Gurren Lagann OST - Sorairo Days FULL VERSION
+bash $ADDTOMPD "-sovoeEVyIo" #Dragon Crisis! (Immoralist -OP Full)
+bash $ADDTOMPD "6yRgj5xQiRo" #Fullmetal Alchemist Brotherhood opening 1 Full Again
+bash $ADDTOMPD "_qlssPLILJE" #[Ore no Imouto] Hanazawa Kana - Masquerade (ED 7 Full) 
 
 mpc play
 
