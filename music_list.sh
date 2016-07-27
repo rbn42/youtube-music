f570cb788df76c3e371f768f41446514c368cd0d
rbn42 <bl100@students.waikato.ac.nz>
2016-07-28 07:15:53 +1200

    進撃の巨人

--- music_list.sh
+++ music_list.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 export ADDTOMPD=$(dirname "$0")/add_to_mpd.sh
-mpc clear
+#mpc clear
 
 bash $ADDTOMPD "p2vxlZkfW-Y" #Record of Lodoss War - opening eternity
 bash $ADDTOMPD "eJiKEy4rsvA" #Record of Lodoss War - opening  ?
 bash $ADDTOMPD "aoEvKKAhSqQ" #Sidonia (Knights of Sidonia Theme Song)
+bash $ADDTOMPD "2IazvA6R0pY" #進撃の巨人 OP 2 FULL- Shingeki no Kyojin Opening 2 FULL HD
 
 mpc play
 
