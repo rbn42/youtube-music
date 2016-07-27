3727c626b964f6bc44504dabf02eedf34eb18b11
rbn42 <bl100@students.waikato.ac.nz>
2016-07-28 07:11:32 +1200

    sidonia

--- music_list.sh
+++ music_list.sh
@@ -4,6 +4,7 @@ mpc clear
 
 bash $ADDTOMPD "p2vxlZkfW-Y" #Record of Lodoss War - opening eternity
 bash $ADDTOMPD "eJiKEy4rsvA" #Record of Lodoss War - opening  ?
+bash $ADDTOMPD "aoEvKKAhSqQ" #Sidonia (Knights of Sidonia Theme Song)
 
 mpc play
 
